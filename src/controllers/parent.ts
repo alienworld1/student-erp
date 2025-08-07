0d6baaf4c2cefbe75981e3f29204e8820c399120
Jaron C E <144526969+Jaroncoder@users.noreply.github.com>
2025-08-07 22:52:36 +0530

    added parent.ts in controller and modified parent.ts in schema

---
+++ src/controllers/parent.ts
@@ -0,0 +1,55 @@
+import asyncHandler from 'express-async-handler';
+import prisma from '../utils/prisma';
+import * as bcrypt from 'bcrypt';
+
+import { ParentSchema } from '../schema/parent';
+
+export const fetchAllParents = asyncHandler(async (req, res) => {
+  const parents = await prisma.parent.findMany();
+  res.json(parents);
+});
+
+export const fetchParentById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const parent = await prisma.parent.findUnique({
+    where: { id },
+  });
+
+  if (!parent) {
+    res.status(404).json({ message: 'Parent not found' });
+    return;
+  }
+
+  res.json(parent);
+});
+
+export const createParent = asyncHandler(async (req, res) => {
+  const parent = ParentSchema.safeParse(req.body);
+
+  if (!parent.success) {
+    res.status(400).json({ issues: parent.error.issues });
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(parent.data.password, 10);
+
+  const newParent = await prisma.parent.create({
+    data: {
+      students: {
+        connect: { id: parent.data.studentid.toString() }
+      },
+      password: hashedPassword,
+      father_name: parent.data.fatherName,
+      mother_name: parent.data.motherName,
+      father_work: parent.data.fatherOccupation,
+      mother_work: parent.data.motherOccupation,
+      father_income: parent.data.fatherIncome,
+      mother_income: parent.data.motherIncome,      
+      email: parent.data.parentEmail,
+      contact: [parent.data.parentMobileNumber],
+        
+    },
+  });
+
+  res.status(201).json(newParent);
+});
--- src/schema/parent.ts
+++ src/schema/parent.ts
@@ -7,13 +7,13 @@ const Parent =z.object({
     motherName: z.string().min(1, 'Mother\'s name is required'),
     fatherOccupation: z.string().min(1, 'Father\'s occupation is required'),
     motherOccupation: z.string().min(1, 'Mother\'s occupation is required'),
-    annualIncome: z.number().min(0, 'Annual income must be at least 0'),
+    fatherIncome: z.number().min(0, 'Annual income must be at least 0'),
+    motherIncome: z.number().min(0, 'Annual income must be at least 0'),
     parentEmail: z.email('Invalid parent email format'),
     parentMobileNumber: z
       .string()
       .min(10, 'Parent mobile number must be at least 10 characters long')
       .max(15, 'Parent mobile number must be at most 15 characters long'),
-    parentAddress: z.string().min(1, 'Parent address is required'),    
 });
 
 export type ParentType = z.infer<typeof Parent>;
