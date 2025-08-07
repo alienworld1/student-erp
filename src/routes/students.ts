746666b3c8ff6c39d77739ed258a50d5e9c7fbf4
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2025-08-07 17:24:52 +0000

    Add ability to add a parent to a student

--- src/routes/students.ts
+++ src/routes/students.ts
@@ -3,12 +3,17 @@ import {
   fetchAllStudents,
   fetchStudentById,
   createStudent,
+  addParent,
 } from '../controllers/student';
 
 const studentRouter = Router();
 
 studentRouter.get('/', fetchAllStudents);
 studentRouter.get('/:id', fetchStudentById);
+
+// parentId is expected as a query parameter
+studentRouter.post('/:id/parent', addParent);
+
 studentRouter.post('/', createStudent);
 
 export default studentRouter;
